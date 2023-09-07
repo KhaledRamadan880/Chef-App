a474945e8d5f934109dab0028f70827ed6a5f563
Khaled Ramadan <khaledramdan880@gmail.com>
2023-09-07 20:30:53 +0300

    Shimmer

--- lib/features/menu/presentation/components/meal_item_component.dart
+++ lib/features/menu/presentation/components/meal_item_component.dart
@@ -3,10 +3,12 @@ import 'package:chef_app/core/util/color.dart';
 import 'package:chef_app/core/util/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shimmer/shimmer.dart';
 
 class CustomMealItem extends StatelessWidget {
   const CustomMealItem({
-    super.key, required this.imageUrl,
+    super.key,
+    required this.imageUrl,
   });
   final String imageUrl;
   @override
@@ -23,7 +25,18 @@ class CustomMealItem extends StatelessWidget {
             height: 60.h,
             child: CachedNetworkImage(
               imageUrl: imageUrl,
-              placeholder: (context, url) => const CircularProgressIndicator(),
+              placeholder: (context, url) => Shimmer.fromColors(
+                baseColor: AppColors.grey,
+                highlightColor: AppColors.white,
+                child: Container(
+                  width: 60.w,
+                  height: 60.h,
+                  decoration: BoxDecoration(
+                    color: AppColors.grey,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+              ),
               errorWidget: (context, url, error) => const Icon(Icons.error),
             ),
           ),
